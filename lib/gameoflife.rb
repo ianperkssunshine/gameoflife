9c37b7f7bc32af6c466f562d71fd186266038fed
Ian Perks <ian.p@sunshine.co.uk>
2018-10-25 15:31:08 +0100

    adjusted two files

--- lib/gameoflife.rb
+++ lib/gameoflife.rb
@@ -1,7 +1,89 @@
 class GameOfLife
 
+
+	def cellalive 
+
+		#alive = [5.5, 4.3, 2.2, 4.5, 8.9]
+
+		alive = [[5,5],[4,3],[2,2],[4,5],[8,9]]
+	end
+
 	def test
 		1
 	end
+
+	def grid
+		[1, 2, 3]
+	end
+
+	def checkOne
+		false
+	end	
+
+	def checkTwo
+		true
+	end		
+
+	def checkNeighbours(n)
+		if n == 1
+			false
+		else
+			true
+		end
+	end		
+
+
+	def aliveCells(n)
+		cellalive.include?(n)
+		
+
+	end	
+
+	def areNeighbours(n)
+
+		x = n[0]
+		y = n[1]
+
+		#[4.4, 4.6, 3.5, 5.5]
+
+		tl = []
+		tl << x - 1
+		tl << y - 1
+
+		tm = []
+		tm << x
+		tm << y - 1		
+		
+		tr = []
+		tr << x
+		tr << y + 9
+
+
+		cl = n - 1
+		cr = n + 1
+
+		bl = n - 0.9
+		bm = n + 0.1
+		br = n + 1.1
+
+
+		# 		tl = n - 1.1
+		# tm = n - 0.1
+		# tr = n + 0.9
+
+		# cl = n - 1
+		# cr = n + 1
+
+		# bl = n - 0.9
+		# bm = n + 0.1
+		# br = n + 1.1
+
+		[tl, tm, tr, cl, cr, bl, bm, br]
+
+	end
+
+
+
+
 
 end
--- spec/gameoflife_spec.rb
+++ spec/gameoflife_spec.rb
@@ -5,13 +5,34 @@ describe "Game of Life rules" do
 	let(:game) { GameOfLife.new }
 
   it "returns 1" do
-
 	expect( 1 ).to eq( 1 )
+  end
 
+  it "returns an array" do
+	expect( game.grid ).to be_an_instance_of(Array)
+  end
+
+  it "1 neighbour so dies" do
+	expect( game.checkNeighbours(1) ).to eq( false )
+  end
+
+  it "2 neighbours so lives" do
+	expect( game.checkNeighbours(2) ).to eq( true )
   end
 
 
+  it "check with cells are alive" do
+	expect( game.aliveCells(5.5) ).to eq( true )
+	expect( game.aliveCells(6.3) ).to eq( false )
+  end
 
+
+  it "return neighbours" do
+	#expect( game.areNeighbours(4.5) ).to eq( [3.4, 4.4, 5.4, 3.5, 5.5, 3.6, 4.6, 5.6] )
+	expect( game.areNeighbours([4,5]) ).to eq(  )
+
+
+  end
 
 
 end
